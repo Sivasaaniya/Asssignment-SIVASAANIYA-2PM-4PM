e335d08b510bae5aceafa6853c5eae0c3978a21b
Sivasaaniya <sssaaniya3@gmail.com>
2025-12-15 19:38:08 +0530

    updated

---
+++ Day15/practicaltask1-4.js
@@ -0,0 +1,26 @@
+//task1
+const today = new Date();
+console.log("Welcome to my website!");
+console.log("Today's date is: " + today.toDateString());
+
+//task 2
+
+console.log("Name: Aqulin")
+console.log("Age: 21")
+console.log("Course: Full-stack")
+console.log("Country: India")
+console.log("Goal: Nothing")
+
+//task4
+/*
+console.log("Aqulin");
+console.log(21);
+console.log(true);
+console.log({name : "Aqulin" , age : 21, });
+*/
+
+var a=10;
+var b=20;
+console.log(a+b)
+
+
---
+++ Day15/practicetask.js
@@ -0,0 +1,14 @@
+//task 1
+console.log("AQULIN JEBHA S");
+
+//task 2:Single line Comment
+console.log("AKZ")
+
+//task 3: Multiline Comment
+/*
+bla bla blah
+bla bla blah
+*/
+
+//task 4:External Css file
+console.log("linked");
